34e37f7b38b05174e9769c5d457893fc6cacfdc2
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-03-18 16:37:42 -0300

    Delete dependency injection was injected into the program.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace AddressBook
             Console.Title = "Address Book";
             List<string> fields = new List<string>();
             int option = 0;
+            int idContact = 0;
 
             using IHost host = InjectDependencies(args).Build();
                         
@@ -36,18 +37,22 @@ namespace AddressBook
                         Console.Clear();
                         Console.WriteLine(Select(host.Services));
                         Console.Write("Enter the Id: ");
-                        int id = Int32.Parse(Console.ReadLine());
+                        idContact = Int32.Parse(Console.ReadLine());
                         Console.Clear();
-                        Console.WriteLine(SelectById(host.Services, id) + "\n");                        
+                        Console.WriteLine(SelectById(host.Services, idContact) + "\n");                        
                         fields = GetFields();
-                        fields.Insert(0, id.ToString());
+                        fields.Insert(0, idContact.ToString());
                         Update(host.Services, fields);
-                        Console.WriteLine("Operation has been completed! Press any key to return to the menu!");
+                        Console.WriteLine("\nOperation has been completed! Press any key to return to the menu!");
                         Console.ReadKey();                                                
                         break;
                     case 3: 
                         Console.Clear();
-                        Console.WriteLine("Delete method is being developed.\nPress any key to return to the Menu!");
+                        Console.WriteLine(Select(host.Services));
+                        Console.Write("Enter the Id: ");
+                        idContact = Int32.Parse(Console.ReadLine());
+                        Delete(host.Services, idContact);
+                        Console.WriteLine("\nOperation has been completed!\nPress any key to return to the Menu!");
                         Console.ReadKey();
                         break;
                     case 4:
@@ -101,10 +106,12 @@ namespace AddressBook
             .ConfigureServices((_, services) =>
             services.AddTransient<ICreateContact, ContactOperations>()
             .AddTransient<ISelectContacts, ContactOperations>()
-            .AddTransient<IUpdateContact, ContactOperations>()        
+            .AddTransient<IUpdateContact, ContactOperations>()  
+            .AddTransient<IDeleteContact, ContactOperations>()      
             .AddTransient<CreateContactService>()
             .AddTransient<SelectContactService>()
-            .AddTransient<UpdateContactService>());
+            .AddTransient<UpdateContactService>()
+            .AddTransient<DeleteContactService>());
 
         static void Create(IServiceProvider services, List<string> fields)
         {
@@ -122,6 +129,15 @@ namespace AddressBook
 
             UpdateContactService contactService = provider.GetRequiredService<UpdateContactService>();
             contactService.Update(new Contact(Convert.ToInt32(fields[0].ToString()), fields[1], fields[2], fields[3], fields[4], fields[5]));
+        }
+
+        static void Delete(IServiceProvider services, int id)
+        {
+            using IServiceScope serviceScope = services.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+
+            DeleteContactService contactService = provider.GetRequiredService<DeleteContactService>();
+            contactService.Delete(new Contact(id));
         }
 
         static string Select(IServiceProvider services)
